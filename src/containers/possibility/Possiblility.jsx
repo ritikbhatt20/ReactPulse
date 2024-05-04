418dc2748a35e6cb6e692801c00cc61a0dfa8570
ritik <ritikbhatt020@gmail.com>
2024-05-04 19:23:10 +0530

    🚀Basic template of containers done

---
+++ src/containers/possibility/Possiblility.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './possibility.css'
+
+const Possiblility = () => {
+  return (
+    <div>
+      Possibility
+    </div>
+  )
+}
+
+export default Possiblility
---
+++ src/containers/whatGPT3/WHATGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatGPT3.css'
+
+const WHATGPT3 = () => {
+  return (
+    <div>
+      WHATGPT3
+    </div>
+  )
+}
+
+export default WHATGPT3
