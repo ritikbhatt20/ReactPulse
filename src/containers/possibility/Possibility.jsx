c6c7d3409db1eff7fa5b87b91a7c80fd5325ddad
ritik <ritikbhatt020@gmail.com>
2024-05-05 18:27:22 +0530

    🔨Basic app.js done

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './possibility.css'
 
-const Possiblility = () => {
+const Possibilility = () => {
   return (
     <div>
       Possibility
@@ -9,4 +9,4 @@ const Possiblility = () => {
   )
 }
 
-export default Possiblility
+export default Possibilility
